bbb6839f40deee8c6874a26f760d65e30023ad59
Veronika Nevařilová <xnevar00@stud.fit.vutbr.cz>
2023-11-19 13:52:37 +0100

    Add comments to helper_functions.hpp

--- include/helper_functions/helper_functions.hpp
+++ include/helper_functions/helper_functions.hpp
@@ -58,23 +58,204 @@ enum Direction {
     Download
 }; 
 
+/**
+ * @brief Checks if a string contains only digits.
+ *
+ * @param str String to be checked.
+ * @return True if the string contains only digits, false otherwise
+ */
 bool str_is_digits_only(std::string str);
+
+
+/**
+ * @brief Function that is called by the getSingleArgument function, help to get the argument
+ *
+ * @param startIndex Starting index of the argument to be extracted.
+ * @param receivedMessage Original message.
+ * @return The extracted argument as a string.
+ */
 std::string getArgument(int startIndex, std::string receivedMessage);
+
+
+/**
+ * @brief Gets the index of the next argument in the message based on the null byte
+ *
+ * @param startIndex Starting index of the argument to be extracted.
+ * @param receivedMessage Original message.
+ * @return Index of the next argument.
+ */
 int getAnotherStartIndex(int startIndex, std::string receivedMessage);
+
+
+/**
+ * @brief Extracts a single argument from a received message starting from a specific index
+ *        and makes some additional checks.
+ *
+ * @param startIndex Starting index of the argument to be extracted.
+ * @param receivedMessage Original message.
+ * @return The extracted argument as a string.
+ */
 std::string getSingleArgument(int startIndex, std::string receivedMessage); 
+
+
+/**
+ * @brief Gets the opcode from a received message (first two bytes).
+ *
+ * @param receivedMessage Original message.
+ * @return Opcode as an integer.
+ */
 int getOpcode(std::string receivedMessage);
+
+
+/**
+ * @brief Sets the option to the value from the received message.
+ *
+ * @param option Pointer to the option variable, where the value will be stored.
+ * @param optionIndex Pointer to the index of the option in the message.
+ * @param receivedMessage Original message.
+ * @param options_string String containing all the options for printing to stderr after parsing the packet.
+ * @return 0 if the option was set successfully, -1 otherwise.
+ */
 int setOption(int64_t *option, int *optionIndex, std::string receivedMessage, std::string *options_string);
+
+
+/**
+ * @brief Converts an integer to a vector of bytes.
+ *
+ * @param value Integer to be converted.
+ * @return Vector of bytes.
+ */
 std::vector<char> intToBytes(unsigned short value);
+
+
+/**
+ * @brief Gets the IP address from a sockaddr_in structure.
+ *
+ * @param sockaddr sockaddr_in structure.
+ * @return IP address as a string.
+ */
 std::string getIPAddress(const struct sockaddr_in& sockaddr);
+
+
+/**
+ * @brief Prints the information about a received ACK packet to stderr.
+ *
+ * @param src_ip IP address of the sender.
+ * @param src_port Port of the sender.
+ * @param block_id Block number of the ACK packet.
+ */
 void printAckInfo(std::string src_ip, int src_port, int block_id);
+
+
+/**
+ * @brief Prints the information about a received DATA packet to stderr.
+ *
+ * @param src_ip IP address of the sender.
+ * @param src_port Port of the sender.
+ * @param dst_port Port of the receiver.
+ * @param block_id Block number of the DATA packet.
+ */
 void printDataInfo(std::string src_ip, int src_port, int dst_port, int block_id);
+
+
+/**
+ * @brief Prints the information about a received ERROR packet to stderr.
+ *
+ * @param src_ip IP address of the sender.
+ * @param src_port Port of the sender.
+ * @param dst_port Port of the receiver.
+ * @param error_code Error code of the ERROR packet.
+ * @param error_msg Error message of the ERROR packet.
+ */
 void printErrorInfo(std::string src_ip, int src_port, int dst_port, unsigned short error_code, std::string error_msg);
+
+
+/**
+ * @brief Prints the information about a received OACK packet to stderr.
+ *
+ * @param src_ip IP address of the sender.
+ * @param src_port Port of the sender.
+ * @param options String containing all the options.
+ */
 void printOackInfo(std::string src_ip, int src_port, std::string options);
+
+
+/**
+ * @brief Prints the information about a received RRQ/WRQ packet to stderr.
+ *
+ * @param opcode Opcode of the packet.
+ * @param src_ip IP address of the sender.
+ * @param src_port Port of the sender.
+ * @param filepath Requested file.
+ * @param mode Requested mode.
+ * @param options String containing all the options.
+ */
 void printRrqWrqInfo(int opcode, std::string src_ip, int src_port, std::string filepath, std::string mode, std::string options);
+
+
+/**
+ * @brief Gets the local port of a socket.
+ *
+ * @param udpSocket Socket.
+ * @return Port of the socket.
+ */
 int getLocalPort(int udpSocket);
+
+
+/**
+ * @brief Gets the port from a sockaddr_in structure.
+ *
+ * @param sockaddr sockaddr_in structure.
+ * @return Port.
+ */
 int getPort(const struct sockaddr_in& sockaddr);
+
+
+/**
+ * @brief Sets the timeout of a socket.
+ *
+ * @param udp_socket Pointer to the socket.
+ * @param seconds Timeout in seconds.
+ * @return 0 if the timeout was set successfully, -1 otherwise.
+ */
 int setTimeout(int *udp_socket, int seconds);
+
+
+/**
+ * @brief Resends the data to the destination.
+ *
+ * @param udp_socket Socket.
+ * @param destination Destination address.
+ * @param data Data to be resent.
+ * @return 0 if the data were resent successfully, -1 otherwise.
+ */
 int resendData(int udp_socket, sockaddr_in destination, std::vector<char> data);
+
+
+/**
+ * @brief Closes the file and deletes it.
+ *
+ * @param file Pointer to the file.
+ * @param file_path Path to the file.
+ */
 void clean(std::ofstream *file, std::string full_filepath);
+
+
+/**
+ * @brief Gets the size of a file.
+ *
+ * @param root_ditpath Path to the root directory.
+ * @param filepath Path to the file.
+ * @return Size of the file.
+ */
 int getFileSize(std::string root_ditpath, std::string filepath);
+
+
+/**
+ * @brief Checks if there is enough space on the disk for the requested file
+ *
+ * @param root_dirpath Path to the root directory.
+ * @param tsize Size of the file.
+ * @return 1 if there is enough space, 0 otherwise and -1 if error occured.
+ */
 int isEnoughSpace(std::string root_dirpath, int64_t tsize);
